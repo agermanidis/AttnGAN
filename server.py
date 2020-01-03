270bba5172da808140dfb65f87c56526c83e19a5
Anastasis Germanidis <agermanidis@gmail.com>
2020-01-03 12:40:44 -0500

    update to new model sdk

--- server.py
+++ server.py
@@ -3,13 +3,11 @@ import torch
 import numpy as np
 from eval import generate, word_index, models
 from miscc.config import cfg, cfg_from_file
-from runway import RunwayModel
+import runway
+import PIL
 
 
-attngan = RunwayModel()
-
-
-@attngan.setup
+@runway.setup
 def setup():
   cfg_from_file('cfg/coco_attn2.yml')
   cfg.CUDA = torch.cuda.is_available()
@@ -26,13 +24,15 @@ def setup():
   return (wordtoix, ixtoword, text_encoder, netG)
 
 
-@attngan.command('generate', inputs={'caption': 'text'}, outputs={'result': 'image'})
+@runway.command('generate', inputs={'caption': runway.text}, outputs={'result': runway.image})
 def generate_command(model, inp):
   wordtoix, ixtoword, text_encoder, netG = model
   caption = inp["caption"]
   img = generate(caption, wordtoix, ixtoword, text_encoder, netG, False)
+  if img is None:
+    img = PIL.Image.new('RGB', (256, 256), color = 'black')
   return dict(result=img)
-    
+
 
 if __name__ == "__main__":
-  attngan.run()
+  runway.run()
